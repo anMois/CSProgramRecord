cbc5d8ad6fad67a34491e4c39582752d56a6db7b
anMois <kew1672@naver.com>
2024-07-19 14:31:00 +0900

    AddSolution
    두 수의 합

---
+++ CSProgramTest/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace CSProgramTest
+{
+    internal class Program
+    {
+        /// <summary>
+        /// 두 수의 합을 구합니다.
+        /// </summary>
+        public int AddSolution(int num1, int num2)
+        {
+            return num1 + num2; //두 수의 합을 구하기 위해 연산자 '+'기호를 사용한다.
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("!@#$%^&*(\\'\"<>?:;");
+        }
+    }
+}
