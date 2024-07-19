083ca63448c09ca3983390bf29f0a48781106a7d
anMois <kew1672@naver.com>
2024-07-19 14:31:45 +0900

    SubSolution
    두 수의 차

--- CSProgramTest/Program.cs
+++ CSProgramTest/Program.cs
@@ -10,6 +10,15 @@
             return num1 + num2; //두 수의 합을 구하기 위해 연산자 '+'기호를 사용한다.
         }
 
+        /// <summary>
+        /// 두 수의 차를 구합니다.
+        /// </summary>
+        public int SubSolution(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("!@#$%^&*(\\'\"<>?:;");
