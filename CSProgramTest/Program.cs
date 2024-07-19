63e6150aee710691b7cde1aee264c7273d86be4e
anMois <kew1672@naver.com>
2024-07-19 14:51:58 +0900

    Compare
    두 수를 비교, 같으면 1반환 다르면 -1반환

--- CSProgramTest/Program.cs
+++ CSProgramTest/Program.cs
@@ -2,6 +2,7 @@
 {
     internal class Program
     {
+        #region 사칙연산
         /// <summary>
         /// 두 수의 합을 구합니다.
         /// </summary>
@@ -32,6 +33,15 @@
         public int DivSolution(int num1, int num2)
         {
             return num1 / num2; //두 수의 정수 몫을 구하기 위해 연산자 '/'기호를 사용한다.
+        }
+        #endregion
+
+        /// <summary>
+        /// 두 수를 비교합니다. 같으면 1을 반환 다르면 -1을 반환
+        /// </summary>
+        public int CompareSolution(int num1, int num2)
+        {
+            return num1 == num2 ? 1 : -1; //반환값이 1 또는 -1 둘로 나타나기에 조건연산자인 '? :'사용한다.
         }
 
         static void Main(string[] args)
