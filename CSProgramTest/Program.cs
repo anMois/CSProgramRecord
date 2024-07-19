3915990b5c7565973a404f678ec10363aa376852
anMois <kew1672@naver.com>
2024-07-19 14:39:48 +0900

    MulSolution
    두 수의 곱

--- CSProgramTest/Program.cs
+++ CSProgramTest/Program.cs
@@ -15,7 +15,15 @@
         /// </summary>
         public int SubSolution(int num1, int num2)
         {
-            return num1 - num2;
+            return num1 - num2; //두 수의 차을 구하기 위해 연산자 '-'기호를 사용한다.
+        }
+
+        /// <summary>
+        /// 두 수의 곱을 구합니다.
+        /// </summary>
+        public int MulSolution(int num1, int num2)
+        {
+            return num1 * num2; //두 수의 곱을 구하기 위해 연산자 '*'기호를 사용한다.
         }
 
 
