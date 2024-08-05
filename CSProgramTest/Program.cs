b25c6c4ec425a07acc7aedbca44971eb35555bba
anMois <kew1672@naver.com>
2024-08-05 16:39:14 +0900

    [Add] Day2
    2일 차 문제 2개 풀고 해당 풀이 과정을 md로 작성

--- CSProgramTest/Program.cs
+++ CSProgramTest/Program.cs
@@ -1,10 +1,10 @@
-﻿namespace CSProgramTest
+﻿namespace Day_1
 {
-    internal class Program
+    public class Program
     {
-        #region 사칙연산
+        #region Day_1
         /// <summary>
-        /// 두 수의 합을 구합니다.
+        /// 정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
         /// </summary>
         public int AddSolution(int num1, int num2)
         {
@@ -12,7 +12,7 @@
         }
 
         /// <summary>
-        /// 두 수의 차를 구합니다.
+        /// 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
         /// </summary>
         public int SubSolution(int num1, int num2)
         {
@@ -20,7 +20,7 @@
         }
 
         /// <summary>
-        /// 두 수의 곱을 구합니다.
+        /// 정수 num1, num2가 매개변수 주어집니다. num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
         /// </summary>
         public int MulSolution(int num1, int num2)
         {
@@ -28,21 +28,13 @@
         }
 
         /// <summary>
-        /// 두 수의 몫을 구합니다. 몫은 정수로 표현합니다.
+        /// 정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
         /// </summary>
         public int DivSolution(int num1, int num2)
         {
             return num1 / num2; //두 수의 정수 몫을 구하기 위해 연산자 '/'기호를 사용한다.
         }
         #endregion
-
-        /// <summary>
-        /// 두 수를 비교합니다. 같으면 1을 반환 다르면 -1을 반환
-        /// </summary>
-        public int CompareSolution(int num1, int num2)
-        {
-            return num1 == num2 ? 1 : -1; //반환값이 1 또는 -1 둘로 나타나기에 조건연산자인 '? :'사용한다.
-        }
 
         static void Main(string[] args)
         {
---
+++ Day_2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Day_2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Divsolution(3,2));
+        }
+
+        /// <summary>
+        /// 정수 num1과 num2가 매개변수로 주어집니다. 
+        /// 두 수가 같으면 1 다르면 -1을 retrun하도록 solution 함수를 완성해주세요.
+        /// </summary>
+        public int CompareSolution(int num1, int num2)
+        {
+            return num1 == num2 ? 1 : -1; //반환값이 1 또는 -1 둘로 나타나기에 조건연산자인 '? :'사용한다.
+        }
+
+        /// <summary>
+        /// 정수 num1과 num2가 매개변수로 주어질 때, num1을 num2로 나눈 값에
+        /// 1,000을 곱한 후 정수 부분을 return 하도록 soltuion 함수를 완성해주세요.
+        /// </summary>
+        public static int Divsolution(int num1, int num2)
+        {
+            float div = (float)num1 / num2;
+
+            return (int)(div * 1000);
+        }
+    }
+}
